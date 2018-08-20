a245e3d4d200894dd73b426d2dee6091b2242344
scion <scion@scion-mbp.local>
2018-08-20 12:37:21 +0900

    Proxy.sol is added

--- RayonBase.sol
+++ RayonBase.sol
@@ -17,5 +17,7 @@ contract RayonBase is Claimable, HasNoContracts, HasNoEther {
         _contractInst.transferOwnership(owner);
     }
 
-    function kill() external;
+    // function kill() external onlyOwner {
+    //     selfdestruct(owner);
+    // }
 }
